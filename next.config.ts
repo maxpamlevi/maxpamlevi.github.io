32480a9d91900da9078c3141194b4db65471f760
jason <khangnd2056@gmail.com>
2026-01-29 10:16:14 +0700

    update NextJs v16

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    devIndicators: false,
-    output: "export"
+    /* config options here */
 };
 
 export default nextConfig;
